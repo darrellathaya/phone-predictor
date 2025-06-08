36deccc7f100a19286fbe6d247a5e06728884d2a
darrellathaya <darrellathayaa@gmail.com>
2025-06-08 15:08:21 +0700

    FastAPI server fix

--- webapp/app/app.py
+++ webapp/app/app.py
@@ -155,4 +155,4 @@ def form_post(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
